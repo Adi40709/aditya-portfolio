b4fb8b4b1d7c882742704a965fb9d7dbaa99b1fe
Aditya <aditya@zylitix.ai>
2026-03-11 16:14:10 +0530

    feat : Update UI

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -10,8 +10,8 @@ const Contact = () => {
           <div className="contact-box">
             <h4>Email</h4>
             <p>
-              <a href="mailto:rajeshchittyal21@gmail.com" data-cursor="disable">
-                rajeshchittyal21@gmail.com
+              <a href="mailto:sharmaaditya1718@gmail.com" data-cursor="disable">
+                sharmaaditya1718@gmail.com
               </a>
             </p>
             <h4>Education</h4>
@@ -54,7 +54,7 @@ const Contact = () => {
           </div>
           <div className="contact-box">
             <h2>
-              Designed and Developed <br /> by <span>Rajesh Chityal</span>
+              Designed and Developed <br /> by <span>Aditya Sharma</span>
             </h2>
             <h5>
               <MdCopyright /> 2025
