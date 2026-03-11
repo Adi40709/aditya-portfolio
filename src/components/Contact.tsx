8e903fca240f12fad67b3b21f6fb23e6e99b44d8
Aditya <aditya@zylitix.ai>
2026-03-11 16:19:38 +0530

    feat : Update Profile

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -28,7 +28,7 @@ const Contact = () => {
               Github <MdArrowOutward />
             </a>
             <a
-              href="https://www.linkedin.com/in/rajesh-chityal-2a70141b3"
+              href="https://www.linkedin.com/in/Aditya-chityal-2a70141b3"
               target="_blank"
               data-cursor="disable"
               className="contact-social"
@@ -44,7 +44,7 @@ const Contact = () => {
               Twitter <MdArrowOutward />
             </a>
             <a
-              href="https://www.instagram.com/therajeshchityal"
+              href="https://www.instagram.com/theAdityachityal"
               target="_blank"
               data-cursor="disable"
               className="contact-social"
